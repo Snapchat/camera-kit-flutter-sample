dff9a3905fa9590570ca26bc3b7f284d1b005df1
Arash Pessian <apessian@snapchat.com>
2023-04-26 09:51:45 -0700

    Add iOS MVP

---
+++ ios/Runner/Configuration.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+public enum Configuration {
+    static let lensGroupId = "REPLACE-THIS-WITH-YOUR-OWN-APP-SPECIFIC-VALUE"
+    static let applicationId = "REPLACE-THIS-WITH-YOUR-OWN-APP-SPECIFIC-VALUE"
+    static let apiToken = "REPLACE-THIS-WITH-YOUR-OWN-APP-SPECIFIC-VALUE"
+}
+
