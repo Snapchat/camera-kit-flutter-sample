8d014c3ccd7203dda7a5433d286d4fe9901d0c08
Ankit Baphna <abaphna@snapchat.com>
2023-09-12 14:47:15 -0700

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/camerakit/flutter/sample/simple/MainActivity.kt
+++ android/app/src/main/kotlin/com/camerakit/flutter/sample/simple/MainActivity.kt
@@ -14,8 +14,8 @@ import java.io.File
 
 class MainActivity : FlutterFragmentActivity() {
 
-    private val CAMERAKIT_GROUP_ID = "59f98a84-6910-45c0-8692-4edd5cc3d5ed" //TODO fill group id here
-    private val CAMERAKIT_API_TOKEN = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjU1MzM1NzM0LCJzdWIiOiIxOTFkNWRmMC1kNWJmLTQ2OGItYTYzNC04NDhlZmRmYWY1OGF-U1RBR0lOR35kMWRmOTE0Yy04ZGI0LTQzZDktYjhmNi1mZDdjOTM2ODQ4NmEifQ.XsUvnwigfWjEFpd6-BqbpabjDCMLK80SvKIrag_iSD4" //TODO fill api token here
+    private val CAMERAKIT_GROUP_ID = "" //TODO fill group id here
+    private val CAMERAKIT_API_TOKEN = "" //TODO fill api token here
     private val CHANNEL = "com.camerakit.flutter.sample.simple"
 
     private lateinit var _result: MethodChannel.Result
